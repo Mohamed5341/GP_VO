fe4d42b409a7a672438e4d2a703edc4b731b33de
Mohamed Ashraf <ashrafmohamed534@gmail.com>
2023-03-22 03:05:38 +0200

    edit ros setup script

--- src/bash/setup_ros_pack.bash
+++ src/bash/setup_ros_pack.bash
@@ -1,5 +1,5 @@
 DONT_RUN=1 make px4_sitl_default gazebo
-source ~/catkin_ws/devel/setup.bash    # (optional)
-source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+source ~/catkin_ws/devel/setup.bash    # (optional if you have ROS worksapce)
+source Tools/simulation/gazebo-classic/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/simulation/gazebo-classic/sitl_gazebo-classic
